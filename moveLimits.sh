ff9203c376cbedf7be3ea22d2d0b084cfbba9b47
Rahul Vats <r1990v@gmail.com>
2019-03-15 17:02:36 +0530

    Create moveLimits.sh

---
+++ moveLimits.sh
@@ -0,0 +1,41 @@
+scp -r limits.conf root@IP:/etc/security/
+
+scp -r limits.conf root@IP:/etc/security/
+
+scp -r limits.conf root@IP:/etc/security/;
+
+scp -r limits.conf root@IP:/etc/security/;
+
+scp -r limits.conf root@IP:/etc/security/;
+
+scp -r limits.conf root@IP:/etc/security/;
+
+scp -r limits.conf root@IP:/etc/security/;
+
+scp -r limits.conf root@IP:/etc/security/;
+
+scp -r limits.conf root@IP:/etc/security/;
+
+scp -r limits.conf root@IP:/etc/security/;
+
+scp -r limits.conf root@IP:/etc/security/;
+
+scp -r limits.conf root@IP:/etc/security/;
+
+scp -r limits.conf root@IP:/etc/security/;
+
+scp -r limits.conf root@IP:/etc/security/;
+
+scp -r limits.conf root@IP:/etc/security/;
+
+scp -r limits.conf root@IP:/etc/security/;
+
+scp -r limits.conf root@IP:/etc/security/;
+
+scp -r limits.conf root@IP:/etc/security/;
+
+scp -r limits.conf root@IP:/etc/security/;
+
+scp -r limits.conf root@IP:/etc/security/;
+
+scp -r limits.conf root@IP:/etc/security/;
